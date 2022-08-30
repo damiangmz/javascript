8311dd2f6c06fc4f07908b11f7ff0f6af72b9e1d
damian <damiangomez.a@hotmail.com>
2022-08-30 16:04:56 -0300

    prueba2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-let dolar = 138.60;
-let euro =138.91;
+/*let dolar = 138.60;
+let euro =138.91;*/
 
 
 
@@ -30,7 +30,7 @@ else if (ingresarMoneda = "euro") {
 conversion()
 conversion2()
 */
-let ingresarMoneda = prompt("Ingrese la moneda a la que desea convertir ");
+/*let ingresarMoneda = prompt("Ingrese la moneda a la que desea convertir "); 
 function conversionDolar() {
     if (ingresarMoneda = "dolar"){
         let valor = parseInt(prompt("ingrese un numero"));
@@ -48,6 +48,26 @@ function conversionEuro() {
     }
     
 }
-conversionEuro()
+conversionEuro()*/
 
+function conversion (){
+    let dolar = 138.60;
+    let euro = 138.91;
+    let resultado = 0;
+    let ingresarMoneda = prompt("Ingrese la moneda a la que desea convertir ");
+    if (ingresarMoneda = "dolar") {
+        let valor = parseInt(prompt("ingrese un numero"));
+        resultado = valor / dolar;
+        alert("el cambio de pesos a dolar es: $" + resultado.toFixed(2))
+    }
+    else if (ingresarMoneda = "euro"){
+        let valor = parseInt(prompt("ingrese un numero"));
+        resultado = valor / euro;
+        alert("el cambio de pesos a euro es: $" + resultado.toFixed(2));
+    }
+    else{
+        alert("Ingrese dolar o euro")
+    }
+}
+conversion()
 
