e4a3cf07f39c1a66b8bf905bb67e023d6f4ad146
damian <damiangomez.a@hotmail.com>
2022-09-05 23:44:31 -0300

    objeto-array

--- js/index.js
+++ js/index.js
@@ -1,54 +1,3 @@
-/*let dolar = 138.60;
-let euro =138.91;*/
-
-
-
-/*function conversion() {
-
-    valor = parseInt(prompt("ingrese un numero"));
-
-    resultado = valor / dolar;
-    alert(" el cambio de pesos a dolares es : $" + resultado)
-}*/
-
-
-/*let ingresarMoneda = prompt("Ingrese la moneda a la que desea convertir ");
-if (ingresarMoneda = "dolar") {
-    function conversion() {
-        valor = parseInt(prompt("ingrese un numero"));
-        resultado = valor / dolar;
-        alert(" el cambio de pesos a dolares es : $" + resultado);   
-    }
-}
-else if (ingresarMoneda = "euro") {
-    function conversion2() {
-        valor = parseInt(prompt("ingrese un numero"));
-        resultado = valor / euro;
-        alert(" el cambio de pesos a euro es : $" + resultado);   
-    }
-}
-conversion()
-conversion2()
-*/
-/*let ingresarMoneda = prompt("Ingrese la moneda a la que desea convertir "); 
-function conversionDolar() {
-    if (ingresarMoneda = "dolar"){
-        let valor = parseInt(prompt("ingrese un numero"));
-        let resultado = valor / dolar ;
-        alert(" el cambio de pesos a dolar es: $" + resultado);
-    }
-    
-}
-conversionDolar()
-function conversionEuro() {
-    if (ingresarMoneda = "euro"){
-        let valor = parseInt(prompt("ingrese un numero"));
-        let resultado = valor / euro ;
-        alert(" el cambio de pesos a euro es: $" + resultado);
-    }
-    
-}
-conversionEuro()*/
 
 function conversion (){
     let dolar = 138.60;
@@ -70,4 +19,21 @@ function conversion (){
     }
 }
 conversion()
+
+class CrearMoneda{  //crear objeto
+    constructor(nombre, cotizacion){
+        this.nombre = nombre;
+        this.cotizacion = cotizacion;
+    }
+
+}
+let moneda1 = new CrearMoneda("peso chileno" , 15.92);
+let moneda2 = new CrearMoneda("real" , 139.67)
+
+console.log(moneda1);
+console.log(moneda2);
+const array = []; //agregando array
+array.push(moneda1 , moneda2);
+
+console.log(array);
 
