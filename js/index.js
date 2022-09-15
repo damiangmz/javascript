99a05e6bbd5ae5a963ec8c5a25b245932d9cc6f8
damian <damiangomez.a@hotmail.com>
2022-09-14 22:37:50 -0300

    entrega evento

--- js/index.js
+++ js/index.js
@@ -1,66 +1,32 @@
-
-/*function conversion (){
-    let dolar = 138.60;
-    let euro = 138.91;
-    let resultado = 0;
-    let ingresarMoneda = prompt("Ingrese la moneda a la que desea convertir ");
-    if (ingresarMoneda === "dolar") {
-        let valor = parseInt(prompt("ingrese un numero"));
-        resultado = valor / dolar;
-        alert("el cambio de pesos a dolar es: $" + resultado.toFixed(2))
-    }
-    else if (ingresarMoneda === "euro"){
-        let valor = parseInt(prompt("ingrese un numero"));
-        resultado = valor / euro;
-        alert("el cambio de pesos a euro es: $" + resultado.toFixed(2));
-    }
-    else{
-        alert("Ingrese dolar o euro")
-    }
-}
-conversion()
-
-class CrearMoneda{  //crear objeto
-    constructor(nombre, cotizacion){
-        this.nombre = nombre;
-        this.cotizacion = cotizacion;
-    }
-
-}
-let pesoChileno = new CrearMoneda("peso chileno" , 15.92);
-let real = new CrearMoneda("real" , 139.67)
-
-const MonedasCreadas = []; //array para acumular monedas creadas
-
-
-
-MonedasCreadas.push(pesoChileno); //sumado al array 
-MonedasCreadas.push(real);
-console.log(MonedasCreadas);
-
-const nombreMonedas = MonedasCreadas.map((el) => el.nombre); //MAP para ver nombres de monedas creadas
-console.log(nombreMonedas);
-
-const filtrarMoneda = MonedasCreadas.filter((el) => el.nombre.includes('peso chileno')); //filtrar moneda con nombre peso chileno 
-console.log(filtrarMoneda);*/
-let dolar = 138.60;
+let dolar = 138.6;
 let euro = 138.91;
 let real = 139.67;
-let PesoChileno = 15.92
+let PesoChileno = 15.92;
 
-
-const importe = document.querySelector("#importe");
+const importe = document.querySelector("#importe"); //Interaccion con el dom
 const valor = document.querySelector("#valor");
-importe.oninput = () => { //entrega de DOM
-    texto.innerHTML = importe.value
+importe.oninput = () => {
+  //entrega de DOM
+  texto.innerHTML = importe.value;
+};
+
+function convertir() {
+  //funcion para convertir monedas
+  let valor = parseInt(document.getElementById("importe").value);
+  let resultado = 0;
+  if (document.getElementById("dolar").checked) {
+    resultado = valor / dolar;
+    alert("el resultado es de: $" + resultado.toFixed(2));
+  }
+  else if (document.getElementById("euro").checked){
+    resultado = valor / euro;
+    alert("el resultado es de: $" + resultado.toFixed(2));
+  }
+  else if(document.getElementById("real").checked){
+    resultado = valor/ real;
+    alert("el resultado es de: $" + resultado.toFixed(2));
+
+  }
 }
-let de = document.getElementById("de");
-let a = document.getElementById("a");
-if (de == 1 && a == 2) {
-    let resultado = importe / dolar;
-    alerte (resultado)
-}
-
-
-
-
+let boton = document.getElementById("cotizador"); //Incorporacion de evento click
+boton.addEventListener("click", convertir);
